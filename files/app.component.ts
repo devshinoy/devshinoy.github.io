ba567442216be5811a693cd1128bdc1c158ac4b4
devshinoy <itsmeshinoy@gmail.com>
2016-10-11 06:47:56 +0530

    Add files via upload

---
+++ files/app.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { TaskService } from './task.service';
+import { bootstrap } from '@angular/platform-browser-dynamic'
+@Component({
+	selector: 'my-app',
+	template: `<h1>Todo-list App</h1>
+        <input type="text" [(ngModel)]="newTask.name" />
+        <input type="date" [(ngModel)]="newTask.dueDate" />
+        <button class="btn btn-success"(click)="addTask(newTask)">Add task</button>
+        <table class="table-striped">
+            <thead>
+                <tr>
+                    <th>Task</th>
+                    <th>Status</th>
+                    <th>Due date</th>
+                    <th></th>
+                </tr>
+            </thead>    
+            <tbody>
+                <tr *ngFor="#task of tasks">
+                    <td>{{task.name }} </td>
+        <td><span [class.done]="task.done">{{task.done ? 'DONE' : 'I\\'M WORKING ON IT'}} </span></td>
+                    <td>{{task.dueDate}}</td>
+                    <td><button class="btn btn-primary" (click)="task.done = !task.done">{{task.done ? 'Undo' : 'Done'}}</button></td>
+                </tr>
+            </tbody>
+        </table>
+ 	`,
+ 	styles:[`
+ 			.done {
+ 				 color: #e26b1e
+ 			}
+ 	`],
+ 	providers: [TaskService]
+})
+
+export class AppComponent {
+	public tasks = [];
+	public newTask = {done:false};
+
+	constructor(private _taskService : TaskService){}
+
+	ngOnInit(){
+		this.getTasks();
+	}
+
+	getTasks(){
+		this._taskService.getTasks().then(tasks => this.tasks = tasks);
+	}
+
+	addTask(task){
+		if(task){
+			this._taskService.addTask(task);
+			this.newTask = {done: false};
+		}
+	}
+ }
+ bootstrap(AppComponent)
---
+++ files/mock-tasks.ts
@@ -0,0 +1,19 @@
+import {Task} from './task';
+
+export var TASKS: Task[] = [
+		{
+			name: 'Task1',
+			done: false,
+			dueDate: '20-10-2016'
+		},
+		{
+			name: 'Task2',
+			done: true,
+			dueDate: '21-10-2016'	
+		},
+		{
+			name: 'Task3',
+			done: false,
+			dueDate: '22-10-2016'	
+		}
+];
