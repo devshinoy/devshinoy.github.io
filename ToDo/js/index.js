6bc8e333982fa82cb5d6525efc2906514e83fdff
devshinoy <itsmeshinoy@gmail.com>
2016-10-14 18:42:44 +0530

    Update index.js

--- ToDo/js/index.js
+++ ToDo/js/index.js
@@ -1,24 +1,32 @@
  function AppComponent() {
         this.todos = [];
         this.addTodo = function(todo) {
-            this.todos.push(todo.value);
+			if(todo.value!=="")
+			{
+				this.todos.push(todo.value);
             todo.value = null;
+			}	
             return false;
         }
+
     }
-    
+	function removeTodo(el)
+	{
+		el.parentNode.style.setProperty("text-decoration", "line-through");
+		el.parentNode.removeChild(el);
+	}
     AppComponent.annotations = [
       new angular.ComponentAnnotation({
         selector: 'todo'
       }),
       new angular.ViewAnnotation({
         template: '<h3>Tasks</h3>' +
-                  '<ul  class="collection"><li class="collection-item" *ng-for="#todo of todos">&nbsp;{{ todo }}</li><br/></ul>' +
+                  '<ul  class="collection"><li class="collection-item" *ng-for="#todo of todos"><button class="btn waves-effect waves-light" onclick="removeTodo(this)">X</button>&nbsp;&nbsp;<label>{{ todo }}</label></li><br/></ul>' +
                   '<form (submit)="addTodo(todotext)"><input #todotext><button class="btn waves-effect waves-light" type="submit">add</button></form>',
         directives: [angular.NgFor]
       })
     ];
-    
+
     document.addEventListener('DOMContentLoaded', function() {
       angular.bootstrap(AppComponent);
     });
