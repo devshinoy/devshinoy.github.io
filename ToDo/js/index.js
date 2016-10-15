51ba5f6f4e8264349625eb7aeac05daa7c833d11
devshinoy <itsmeshinoy@gmail.com>
2016-10-14 20:56:28 -0700

    Update index.js

--- ToDo/js/index.js
+++ ToDo/js/index.js
@@ -21,7 +21,7 @@
       }),
       new angular.ViewAnnotation({
         template: '<h3>Tasks</h3>' +
-                  '<ul  class="collection"><li class="collection-item" *ng-for="#todo of todos"><button class="btn waves-effect waves-light" onclick="removeTodo(this)">X</button>&nbsp;&nbsp;<label><strong>{{ todo }}</strong></label></li><br/></ul>' +
+                  '<ul  class="collection"><li class="collection-item" *ng-for="#todo of todos"><button class="btn waves-effect waves-light blue accent-4" onclick="removeTodo(this)">X</button>&nbsp;&nbsp;<label><b>{{ todo }}</b></label></li><br/></ul>' +
                   '<form (submit)="addTodo(todotext)"><input placeholder="type here" #todotext><button class="btn waves-effect waves-light blue accent-4" type="submit">add</button></form>',
         directives: [angular.NgFor]
       })
