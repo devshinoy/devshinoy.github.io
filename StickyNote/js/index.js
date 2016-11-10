196e81e856e924ebab5a9ada49820b34620f7454
devshinoy <itsmeshinoy@gmail.com>
2016-11-10 10:53:36 +0530

    Update index.js

--- StickyNote/js/index.js
+++ StickyNote/js/index.js
@@ -20,8 +20,8 @@
         selector: 'sticky'
       }),
       new angular.ViewAnnotation({
-        template:'  <div class="fixed-action-btn"><a class="btn-floating btn-large red" onclick="addTodo(notetext)"><i class="large material-icons">mode_edit</i></a></div>' +
-                 '<div class="col s12 l4 m4" *ng-for="#note of notes"><div class="card yellow accent-4"><div class="card-content dark-text"><div class ="row"><div class="input-field col s8"><input value="{{ note }}" type="text" placeholder="note title"></span></div><div class="col s1"><br/></div><div class="col s1"><button class="btn-floating btn-small waves-effect waves-light orange accent-3" onclick="removeTodo(this)">X</button></div></div><div class="input-field"><textarea id="textarea1" class="materialize-textarea" placeholder="notes here"></textarea></div></div></div></div><br/>',
+        template:'<div class="row"><div class="col s12 l4 m4><br/></div><div class="col s12 l4 m4"><form (submit)="addTodo(notetext)"><input id="input_text" type="text" maxlength="20" placeholder="enter the note title" #notetext><button class="btn waves-effect waves-light orange accent-3" type="submit"><b>+</b></button></form></div></div>' +
+                 '<div class="col s12 l4 m4" *ng-for="#note of notes"><div class="card yellow accent-2"><div class="card-content dark-text"><div class ="row"><div class="input-field col s8"><input value="{{ note }}" type="text" placeholder="note title"></span></div><div class="col s1"><br/></div><div class="col s1"><button class="btn-floating btn-small waves-effect waves-light orange accent-3" onclick="removeTodo(this)">X</button></div></div><div class="input-field"><textarea id="textarea1" class="materialize-textarea" placeholder="notes here"></textarea></div></div></div></div><br/>',
         directives: [angular.NgFor]
       })
     ];
