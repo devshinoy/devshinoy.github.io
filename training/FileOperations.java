a7e077fa1ba61597ccd21f0f03066b94284c5307
Shinoy Shaji <itsmeshinoy@gmail.com>
2017-07-20 19:31:48 +0530

    Update FileOperations.java

--- training/FileOperations.java
+++ training/FileOperations.java
@@ -1,6 +1,10 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class FileOperations {
@@ -40,6 +44,7 @@ class FindFile {
 					}
 				}
 			}
+
 		}
 
 		catch (Exception e) {
@@ -48,7 +53,7 @@ class FindFile {
 
 	}
 
-	public void findDoneFile(String fileName, String fileExt, String folderName) {
+	public void findDoneFile(String fileName, String fileExt, String folderName) throws Exception {
 
 		String fileDone = fileName.concat(".done");
 		File name = new File(folderName);
@@ -58,6 +63,34 @@ class FindFile {
 			if (fileN.isFile()) {
 				if (fileName.matches("^.*." + fileExt + "$")) {
 					System.out.println(fileDone);
+					int count = 0;
+					String path = folderName.concat("\\");
+					String pathFile = path.concat(fileName);
+					Scanner sc = new Scanner(new FileReader(pathFile));
+					while (sc.hasNext()) {
+						sc.next();
+						count++;
+					}
+					System.out.println("Total word count is : " + count);
+					Scanner fileRead = new Scanner(new FileReader(pathFile));
+					String[] str = new String[count];
+					int i = 0;
+					while (fileRead.hasNext()) {
+						str[i] = fileRead.next();
+						String ed = str[i];
+						if (ed.matches("^.*ed$")) {
+							System.out.println(ed);
+						}
+						i++;
+					}
+					StringBuilder strBuilder = new StringBuilder();
+					for (i = 0; i < str.length; i++) {
+						strBuilder.append(str[i]);
+						strBuilder.append(" ");
+					}
+					String newString = strBuilder.toString();
+					System.out.println(newString);
+					wordOccurence(newString);
 					break;
 				}
 			}
@@ -66,6 +99,35 @@ class FindFile {
 				System.out.println("Not found");
 			}
 		}
+	}
+
+	void wordOccurence(String strContent) throws Exception {
+
+		Map<String, Integer> repeatedWord = new HashMap<String, Integer>();
+		String[] words = strContent.split(" ");
+		for (String word : words) {
+			String temp = word.toLowerCase();
+			if (repeatedWord.containsKey(temp)) {
+				repeatedWord.put(temp, repeatedWord.get(temp) + 1);
+			} else {
+				repeatedWord.put(temp, 1);
+			}
+		}
+
+		for (Map.Entry<String, Integer> entry : repeatedWord.entrySet()) {
+			System.out.println(entry.getKey() + "\t\t" + entry.getValue());
+		}
+
+		System.out.println("Unique Words Count : ");
+		int count = 0;
+		for (Map.Entry<String, Integer> entry : repeatedWord.entrySet()) {
+			if (entry.getValue() == 1) {
+				count++;
+			}
+		}
+
+		System.out.print(count);
+
 	}
 
 }
